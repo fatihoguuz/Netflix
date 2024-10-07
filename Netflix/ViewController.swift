45319582111d086147a304c6854cdc54e29a754a
Fatih Oguz <oguzfatih39@gmail.com>
2024-10-07 17:14:15 +0300

    Initial Commit

---
+++ Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by Fatih Oğuz on 7.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
